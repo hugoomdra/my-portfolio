cd22bf500336cccaa954be4c8dcd9cd0838a0d82
hugoomdra <hugoomdra@gmail.com>
2023-08-21 01:23:21 +0200

    project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from '@/components/hero-section'
+import Projects from '@/components/projects'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="">
       <HeroSection />
+      <Projects />
     </main>
   )
 }
---
+++ src/components/projects.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Image from 'next/image'
+import React from 'react'
+import { projects } from '@/lib/data'
+import { motion } from 'framer-motion'
+
+export default function Projects() {
+    return (
+        <motion.div 
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+            type: "keyframes",
+            duration: 1,
+            delay: 1.5,
+        }}
+        className='py-16'>
+
+            <div className='px-8 lg:px-0 lg:w-2/3 mx-auto'>
+                
+                <h3 className='text-2xl mb-8 font-bold'>Projets</h3>
+
+                <div className='grid grid-cols-1 lg:grid-cols-3 gap-x-8 gap-y-12'>
+
+                    {projects.map((project) => (
+
+                        <div className='flex flex-col gap-y-4'>
+
+                            <Image src={project.image} alt='' width={1000} height={1000} className='rounded-md'></Image>
+
+                            <p className='font-bold'>{project.name}</p>
+
+                            <p className='text-gray-800 '>{project.description}</p>
+
+                            {project.link && <span><span className='mr-2'>👉</span> <a target='_blank' className='hover:text-gray-500 duration-200' href={project.link}>{project.displayLink}</a></span>}
+
+                        </div>
+                    ))
+                    }
+                </div>
+            </div>
+        </motion.div>
+    )
+}
